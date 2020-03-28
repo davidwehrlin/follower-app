926987ed38134e70b574ffd65dca7b886c5b1ede
davidwehrlin <djwehrlin@gmail.com>
2020-03-28 00:38:34 -0600

    Created canvas window for game camera

---
+++ script.js
@@ -0,0 +1,6 @@
+var canvas = document.getElementById("game")
+canvas.width = 768
+canvas.height = 432
+canvas.style.border = "1px solid #000000"
+var ctx = canvas.getContext("2d")
+console.log(canvas)
